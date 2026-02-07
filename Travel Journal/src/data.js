5e116bf5d612c30c4fd3d13c39534e483d587516
Jatin Satare <jatinsatare@gmail.com>
2026-02-07 22:23:19 +0530

    add location details data to the app

---
+++ Travel Journal/src/data.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of Sydney Harbour, it is one of the most famous buildings in the world."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geirangerfjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway, and is one of the most visited tourist destinations in the country."
+    },
+    {
+        id: 4,
+        img: {
+            src: "https://images.unsplash.com/photo-1505761671935-60b3a7427bad",
+            alt: "Eiffel Tower"
+        },
+        title: "Eiffel Tower",
+        country: "France",
+        googleMapsLink: "https://maps.app.goo.gl/T5gM3D6xB9U2",
+        dates: "10 Apr, 2022 - 20 Apr, 2022",
+        text: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris and is one of the most recognizable landmarks in the world."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://images.unsplash.com/photo-1526778548025-fa2f459cd5c1",
+            alt: "Colosseum"
+        },
+        title: "Colosseum",
+        country: "Italy",
+        googleMapsLink: "https://maps.app.goo.gl/hW2H6HZyHF9",
+        dates: "05 Jun, 2022 - 15 Jun, 2022",
+        text: "The Colosseum is an oval amphitheatre in the centre of Rome and is the largest ancient amphitheatre ever built."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+            alt: "Grand Canyon"
+        },
+        title: "Grand Canyon",
+        country: "United States",
+        googleMapsLink: "https://maps.app.goo.gl/4yLZtBwh4mn",
+        dates: "02 Sep, 2022 - 18 Sep, 2022",
+        text: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona and is known for its visually overwhelming size."
+    }
+]
